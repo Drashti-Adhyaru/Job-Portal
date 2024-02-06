e4375abc53f3ff39eba0faed3f3d0e7f44ddb403
vatsalpatel <vatsal.p1@ahduni.edu.in>
2024-02-06 17:34:21 -0500

    added create

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var database = require('./config/database');
 var bodyParser = require('body-parser');         // pull information from HTML POST (express4)
 var path = require('path'); // importing path
 const clientSessions = require("client-sessions");
+const uuid = require('uuid');
 
 var port = process.env.PORT || 8000;
 app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
@@ -55,6 +56,41 @@ app.engine('hbs', exphbs.engine({
     res.render('all_movies');
     
   });
+
+  app.post('/api/add_user', async function (req, res) {
+    console.log(req.body);
+    try {
+      const user = await Users.create({
+        _id: uuid.v4(),
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        phone: req.body.phone,
+        role: req.body.role,
+        password: req.body.password
+
+      });
+      Users.find().exec()
+      .then(data => {
+        console.log(data);
+        res.render('all_movies', { data: data });
+      })
+      .catch(error => {
+        res.status(500).send(error.message);
+      });
+  
+    } catch (error) {
+      res.status(500).send(error.message);
+  
+    }
+  });
+
+
+
+
+
+
+
 
 app.listen(port);
 console.log("App listening on port : " + port);
