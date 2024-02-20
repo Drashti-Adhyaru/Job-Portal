ee68e926aa58384ffa608de5b02e98d30810aa78
Drashti-Adhyaru <n01539964@humbermail.ca>
2024-02-19 22:10:05 -0500

    added desc

--- src/components/Jobss.tsx
+++ src/components/Jobss.tsx
@@ -1,30 +1,36 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-async function Jobss() {
 
 
-  type JobType = {
-    _id: string,
-    userId: string,
-    title: string,
-    companyName: string,
-    description: string,
-    type: string,
-    pay: number,
-    category: string,
-    address: string
-  }
+type JobType = {
+  _id: string,
+  userId: string,
+  title: string,
+  companyName: string,
+  description: string,
+  type: string,
+  pay: number,
+  category: string,
+  address: string
+}
 
-  const [jobs, setJobs] = useState<JobType[]>([]);
+  
+
+
+
+
+ async function Jobss() {
+  const [jobs, setJobs] = useState<any[]>([]);
 
   useEffect(() => {
     async function getJobs() {
       try {
-        const response = await axios.get("/api/jobs");
-        setJobs(response.data.data); // Assuming the data you need is in the response's data property
-        console.log(response.data.data);
-        console.log(jobs.length);
+        const response = await axios.get("/api/jobs/");
+        if (response.status === 200) {
+          console.log(response.data); // Make sure the data structure is as expected
+          setJobs(response.data.data); // Set the fetched data to the state
+        }
       } catch (error) {
         console.error("Error fetching jobs:", error);
       }
@@ -32,21 +38,57 @@ async function Jobss() {
 
     getJobs();
   }, []);
+
+
+
+
+  // const [jobs, setJobs] = useState([]);
+
+  // const   getJobs = async() => {
+
+
+
+    // const dynamicData = await fetch(`https://...`, { cache: 'no-store' })
+    // setJobs(dynamicData);
+
+    // try {
+    //   const response = await axios.get("/api/jobs");
+    //   if(response.status==200){
+    //     console.log(response.data.data);
+    //     setJobs(response.data.data.category); // Assuming the data you need is in the response's data property
+    //     console.log(jobs);
+    //   }
+      
+    // } catch (error) {
+    //   console.error("Error fetching jobs:", error);
+    // }
+  // };
+  // useEffect(() => {
+  //   getJobs();
+  // }, []);
   return (
     <>
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@iconscout/unicons@3.0.6/css/line.css"
       />
+{jobs.map((job) => (
+        <div className="text-black" >
+          {job.description}
+        </div>
+      ))}
 
 
-      {jobs.map((job) => {
+
+
+
+      {/* {jobs.map((job) => {
             <div className="text-black">
-              hello
+              {job.category}
             </div>
    
 
-      })}
+      })} */}
 
 
 
